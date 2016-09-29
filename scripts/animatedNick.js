bd006951f7e974cc5f96596b9ecbb867a963bbac
xDefcon <xdefconhd@gmail.com>
2016-09-29 17:29:55 +0200

    Ver. 1.4 - Added "!animated mode" and "!animated delay" commands to modify setting via TS3. Uniformed strings with a double quotes indicator.

--- scripts/animatedNick.js
+++ scripts/animatedNick.js
@@ -20,80 +20,79 @@
 
 
 registerPlugin({
-    name: 'Animated Nickname',
-    version: '1.3',
-    description: 'This scripts kicks some fun in your bot, you can set a custom animated nickname or description.',
-    author: 'Luigi M. - xDefcon (xdefconhd@gmail.com)',
+    name: "Animated Nickname",
+    version: "1.4",
+    description: "This scripts kicks some fun in your bot, you can set a custom animated nickname or description.",
+    author: "Luigi M. - xDefcon (xdefconhd@gmail.com)",
     vars: {
         enableSwitch: {
-            title: 'Activate the script?',
-            type: 'select',
-            options: ['no', 'yes']
+            title: "Activate the script?", type: "select", options: ["no", "yes"]
         }, animatedMode: {
-            title: 'Animated mode [nick/desc/both]',
-            type: 'select',
-            options: ['nickname', 'description', 'nickname + description']
+            title: "Animated mode [nick/desc/both]",
+            type: "select",
+            options: ["nickname", "description", "nickname + description"]
         }, delayTime: {
-            title: 'Nick/Desc changer delay [milliseconds]',
-            type: 'number',
-            placeholder: 'Change nick/description every... (Default: 1000ms).'
+            title: "Nick/Desc changer delay [milliseconds]",
+            type: "number",
+            placeholder: "Change nick/description every... (Default: 1000ms)."
         }, customNicks: {
-            title: 'Nicknames list',
-            type: 'string',
-            placeholder: 'A comma separated list of nicks/descs (e.g: BOT 1,BOT 2,BOT 3).'
+            title: "Nicknames list",
+            type: "string",
+            placeholder: "A comma separated list of nicks/descs (e.g: BOT 1,BOT 2,BOT 3)."
         }, adminUUID: {
-            title: 'Command admin UUIDs',
-            type: 'string',
-            placeholder: 'A comma separated list of Unique IDs of admins to run the !animated on/off command'
+            title: "Command admin UUIDs",
+            type: "string",
+            placeholder: "A comma separated list of Unique IDs of admins to run the !animated on/off command"
         }, debugSwitch: {
-            title: 'Enable debug messages?', type: 'select', options: ['no', 'yes']
+            title: "Enable debug messages?", type: "select", options: ["no", "yes"]
         }
     }
 }, function (sinusbot, config) {
     var i = 0;
+    var minimumDelay = 100;
     var adminUUID = [];
     var initialNick = sinusbot.getNick();
-    if (typeof config.enableSwitch == 'undefined') {
+    if (typeof config.enableSwitch == "undefined") {
         config.enableSwitch = false;
     }
-    if (typeof config.animatedMode == 'undefined') {
+    if (typeof config.animatedMode == "undefined") {
         config.animatedMode = 0;
         debug("No mode selected, selecting NICK mode...")
     }
-    if (typeof config.debugSwitch == 'undefined') {
+    if (typeof config.debugSwitch == "undefined") {
         config.debugSwitch = 0;
     }
     if (config.adminUUID) {
-        adminUUID = config.adminUUID.split(',');
+        adminUUID = config.adminUUID.split(",");
         debug(adminUUID.length + " Admin UUIDs loaded.")
     } else {
         debug("No Admin UUIDs found.");
     }
-    if (typeof config.delayTime == 'undefined') {
+    if (typeof config.delayTime == "undefined") {
         config.delayTime = 1000;
         debug("Nick/Desc changer delay set to 1000ms.");
-    } else if (config.delayTime < 100) {
+    } else if (config.delayTime < minimumDelay) {
         debug("To avoid lag/crashes, the delay MUST be greater than 100 milliseconds.");
         config.delayTime = 1000;
         debug("Delay set to 1 second.");
     } else {
         debug("Nick/Desc changer delay set to " + config.delayTime + ".");
     }
-    if (typeof config.customNicks == 'undefined') {
+    if (typeof config.customNicks == "undefined") {
         debug("Nickname/description list empty or wrong, SCRIPT STOPPED.");
         return;
     }
 
-    setInterval(nickChange, config.delayTime);
-    sinusbot.on('chat', function (ev) {
+    var intervalN = setInterval(nickChange, config.delayTime);
+    sinusbot.on("chat", function (ev) {
         var message = ev.msg;
         if (adminUUID.indexOf(ev.clientUid) >= 0) {
             switch (message) {
-                case '!animated on':
+                case "!animated on":
                     config.enableSwitch = 1;
                     debug("Enabling script by command.");
                     break;
-                case '!animated off':
+                case "!animated off":
                     config.enableSwitch = 0;
                     debug("Disabling script by command.");
                     if (config.animatedMode == 0) sinusbot.setNick(initialNick);
@@ -103,7 +102,46 @@ registerPlugin({
                         sinusbot.setNick(initialNick);
                     }
                     break;
+                default:
+                    var split = message.split(" ");
+                    if (split[0] + " " + split[1] == "!animated mode") {
+                        switch (split[2]) {
+                            case "nickname":
+                                config.animatedMode = 0;
+                                sinusbot.chatPrivate(ev.clientId, "Animated mode set to: nickname.");
+                                debug("Animated mode set to: nickname via COMMAND by: " + ev.clientUid);
+                                break;
+                            case "description":
+                                config.animatedMode = 1;
+                                sinusbot.chatPrivate(ev.clientId, "Animated mode set to: description.");
+                                debug("Animated mode set to: description via COMMAND by: " + ev.clientUid);
+                                break;
+                            case "both":
+                                config.animatedMode = 2;
+                                sinusbot.chatPrivate(ev.clientId, "Animated mode set to: nickname + description.");
+                                debug("Animated mode set to: nickname + description via COMMAND by: " + ev.clientUid);
+                                break;
+                            default:
+                                sinusbot.chatPrivate(ev.clientId, "Unknown mode for '" + split[2] + "', valid ones are: 'nickname', 'description' and 'both'.");
+                                break;
+                        }
+                    }
+                    if (split[0] + " " + split[1] == "!animated delay") {
+                        if (!isNaN(split[2])) {
+                            var msgDelay = parseInt(split[2]);
+                            if (msgDelay < minimumDelay) {
+                                sinusbot.chatPrivate(ev.clientId, "To avoid lag/crashes, the delay MUST be greater than 100 milliseconds.");
+                            } else {
+                                config.delayTime = msgDelay;
+                                clearInterval(intervalN);
+                                intervalN = setInterval(nickChange, config.delayTime);
+                                sinusbot.chatPrivate(ev.clientId, "Delay time set to: " + msgDelay + " milliseconds.");
+                                debug("Delay time set to: " + msgDelay +" by: " + ev.clientUid);}
+                        }
+                    }
+                    break;
             }
+
         }
     });
 
@@ -115,7 +153,7 @@ registerPlugin({
 
     function nickChange() {
         if (!config.enableSwitch) return;
-        var nickArr = config.customNicks.split(',');
+        var nickArr = config.customNicks.split(",");
         debug("Found " + nickArr.length + " nickname(s)/description(s).");
         if (i >= nickArr.length) {
             i = 1;

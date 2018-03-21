6d0a4426ab63f4796048cfe4896fa49ca64a4c97
xDefcon <luigi@xdefcon.com>
2018-03-21 15:46:51 +0100

    Update copyright

--- scripts/AntiProxy.js
+++ scripts/AntiProxy.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 Luigi Martinelli <luigi@xdefcon.com>
+ * Copyright (C) 2018 Luigi Martinelli <luigi@xdefcon.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- scripts/nickCrashHelper.js
+++ scripts/nickCrashHelper.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 Luigi Martinelli <xdefconhd@gmail.com>
+ * Copyright (C) 2018 Luigi Martinelli <xdefconhd@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
